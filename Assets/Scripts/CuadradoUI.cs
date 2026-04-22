e17483ab1049f73e358c0428c0ed9fd3376a610e
delfinagamenara <delfigamenara@gmail.com>
2026-04-22 14:22:19 -0300

    Initial commit

---
+++ Assets/Scripts/CuadradoUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CuadradoUI : MonoBehaviour
+{
+    public TMP_InputField input_num1;
+    public TextMeshProUGUI txt_Resultado;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+   public void ShowNumSquare ()
+   {
+    int num1 =  int.Parse(input_num1.text);
+    txt_Resultado.text = (num1 * num1).ToString();
+
+   }
+}
---
+++ Assets/Scripts/RestaUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class RestaUI : MonoBehaviour
+{
+    public TMP_InputField input_num1;
+    public TMP_InputField input_num2;
+    public TextMeshProUGUI txt_Resultado;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+   public void ShowNumSquare ()
+   {
+    int num1 =  int.Parse(input_num1.text);
+    int num2= int.Parse (input_num2.text);
+    txt_Resultado.text = (num1 - num2).ToString();
+
+    if (num1 - num2 <0){
+        txt_Resultado.text = "error";
+    }
+   }
+}
